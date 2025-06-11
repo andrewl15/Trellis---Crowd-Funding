23c8de6a1bc3779719efb7a029b1b704cef39428
Andrew <longandrew321@gmail.com>
2025-06-11 09:28:48 -0400

    added back button

--- java/src/main/java/com/techelevator/controller/CampaignController.java
+++ java/src/main/java/com/techelevator/controller/CampaignController.java
@@ -4,7 +4,6 @@ import java.security.Principal;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import jakarta.validation.Valid;
