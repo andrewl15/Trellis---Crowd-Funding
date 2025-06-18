e3dab92f30036aee24de01659ac0240a265e1d2b
Andrew <longandrew321@gmail.com>
2025-06-17 22:27:34 -0400

    working on home page

--- react/src/views/HomeView/HomeView.jsx
+++ react/src/views/HomeView/HomeView.jsx
@@ -4,6 +4,7 @@ import NavView from '../../components/MainNav/MainNav';
 import { UserContext } from '../../context/UserContext';
 import styles from '../HomeView/HomeView.module.css';
 import { useState } from 'react';
+import image from '../../images/FinaleLogo.png'
 
 
 export default function HomeView() {
@@ -21,11 +22,20 @@ export default function HomeView() {
     <>
       <NavView />
       <div className={styles.mainDiv}>
-        <div>
-
+        <div className={styles.topSection}>
+          <img className={styles.logo} src={image} alt="" />
         </div>
-
-        <div className={styles.createDiv} onClick={handleClick}>
+        <div className={styles.leftSection}>
+          <lord-icon
+            src="https://cdn.lordicon.com/btvefodi.json"
+            trigger="hover"
+            delay="0"
+            colors="primary:#5c230a,secondary:#407440"
+            style={{ width: "100px", height: "100px", margin: "0", padding: "0" }}
+          ></lord-icon>
+          <p>At Trellis we are here to support your dreams to grow your ideas!</p>
+        </div>
+        <div className={styles.middleSection} onClick={handleClick}>
           <lord-icon
             src="https://cdn.lordicon.com/dwhxbrvz.json"
             trigger="hover"
@@ -33,9 +43,13 @@ export default function HomeView() {
             colors="primary:#5c230a,secondary:#407440"
             style={{ width: "200px", height: "200px", margin: "0", padding: "0" }}
           ></lord-icon>
-          <p className={styles.getStarted}>Click to get started</p>
+          <p className={styles.getStarted}>Get Started</p>
         </div>
-
+        <div className={styles.rightSection}>
+          <p>dddddd ddddddd dddddddddddddd dddddddddd ddddddddd ddddd dddddd dddddddd ddddd dddddddd d dddd dd ddd dd dddddd ddddd ddd ddddd ddddd dddd ddd</p>
+        </div>
+        <div className={styles.rightTrellis}></div>
+        <div className={styles.leftTrellis}></div>
       </div>
     </>
 
